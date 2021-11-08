8392e47cc84e2c023f499bfa02abd8274d8fbf9d
juleka <jule@hrdag.org>
2021-11-08 12:01:09 -0500

    adding word count per day as bargraph

--- R-code/clean/src/clean-UN-data.R
+++ R-code/clean/src/clean-UN-data.R
@@ -13,6 +13,7 @@ library(tm)
 library(textclean)
 library(textstem)
 library(stringr)
+library(ggplot2)
 
 setwd("~/git/UsingTextAsDataWorkshop/R-code/")
 #load our data
@@ -43,5 +44,16 @@ c(text_df[text_df$jobno=="N1432775", "text"])
 c(clean_text_df[clean_text_df$jobno=="N1432775", "text"])
 
 #let's save our clean text data file for analysis later on
-write.csv(clean_text_df, file="clean/output/pc-iii-text-clean.csv") 
+write.csv(clean_text_df, file="clean/output/pc-iii-text-clean.csv")
+
+# let's plot the number of words over time, out of pure curiosity
+clean_text_df %>% 
+  arrange(date) %>% 
+  ggplot(aes(date,n_words_doc)) + 
+  geom_col(col="black", fill="#2596be") +
+  theme_bw() + 
+  labs(x=NULL, y="N(words)") +
+  ggtitle("Total number of words per day")
+ggsave("clean/output/bg-total-number-words-day.pdf", height = 4, width = 10)
+
 #end of Rscript.
